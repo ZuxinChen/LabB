0e8f1c5c891180fe5bab87ca0059e26719cf541b
ZuxinChen <116303234+ZuxinChen@users.noreply.github.com>
2022-11-08 21:32:44 -0500

    This is FXML Example

---
+++ src/main/java/com/mycompany/labb/App.java
@@ -0,0 +1,31 @@
+package com.mycompany.labb;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(App.class, (java.lang.String[])null);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            AnchorPane page = (AnchorPane) FXMLLoader.load(App.class.getResource("IssueTrackingLite.fxml"));
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Issue Tracking Lite Sample");
+            primaryStage.show();
+        } catch (Exception ex) {
+           // Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
