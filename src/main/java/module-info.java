fbf17fc8db4a39a20f10fa9d11d808a078051a94
ZuxinChen <116303234+ZuxinChen@users.noreply.github.com>
2022-11-08 22:54:01 -0500

    This is FXML Example for labB

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.labb {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.labb to javafx.fxml;
     exports com.mycompany.labb;
